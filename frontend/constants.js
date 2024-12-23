578a51d34504d9d2bdcd5a03dbb0556714593722
gatopotato <samkitjai09@gmail.com>
2024-12-24 05:00:43 +0530

    api changes

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,3 +1,3 @@
-const BASE_URL = "http://localhost:80";
+const BASE_URL = "http://54.252.161.213/api";
 
 export const API_URL = BASE_URL + "/api";
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:80/api";
+const API_URL = import.meta.env.VITE_API_URL || "http://54.252.161.213/api";
 
 const api = axios.create({
   baseURL: API_URL,
